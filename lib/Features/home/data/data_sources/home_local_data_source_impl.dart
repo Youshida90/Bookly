6dace0c385e95cc336560bd560e5b4b544dc9486
Youshida90 <youshidacompaney@gmail.com>
2024-12-21 22:36:20 +0000

    add box to the home local feach featured books

--- lib/Features/home/data/data_sources/home_local_data_source_impl.dart
+++ lib/Features/home/data/data_sources/home_local_data_source_impl.dart
@@ -1,11 +1,13 @@
 import 'package:bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/constants/constants.dart';
+import 'package:hive/hive.dart';
 
-class HomeLocalDataSourceImpl extends HomeLocalDataSource{
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(Constants.kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:bookly_app/core/constants/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   runApp(const BooklyApp());
   Hive.registerAdapter(BookEntityAdapter());
+  await Hive.initFlutter();
   await Hive.openBox(Constants.kFeaturedBox);
 }
 
